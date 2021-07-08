f31c4b9d5240631ba2fff94760690553cb055ce5
glenadamson1984 <55412448+glenadamson1984@users.noreply.github.com>
2021-07-08 09:20:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Map from "./Map";
 import { Layers, TileLayer, VectorLayer } from "./Layers";
 import { Style, Icon } from "ol/style";
@@ -15,7 +15,7 @@ import mapConfig from "./config.json";
 import "./App.css";
 
 const geojsonObject = mapConfig.geojsonObject;
-const geojsonObject2 = mapConfig.geojsonObject2;
+const geojsonObject2 = mapConfig.geojsonObject2
 const markersLonLat = [mapConfig.kansasCityLonLat, mapConfig.blueSpringsLonLat];
 
 function addMarkers(lonLatArray) {
@@ -39,7 +39,7 @@ function addMarkers(lonLatArray) {
 const App = () => {
   const [center, setCenter] = useState(mapConfig.center);
   const [zoom, setZoom] = useState(9);
-
+    const [object2, setObject2] = useState(geojsonObject2);
   const [showLayer1, setShowLayer1] = useState(true);
   const [showLayer2, setShowLayer2] = useState(true);
   const [showMarker, setShowMarker] = useState(false);
@@ -48,83 +48,90 @@ const App = () => {
 
   console.log(osm());
   const states = new TileWMS({
-    url: 'https://ahocevar.com/geoserver/wms',
-    params: {'LAYERS': 'topp:states', 'TILED': true},
-    serverType: 'geoserver',
-    // Countries have transparency, so do not fade tiles:
-    transition: 0,
-  });
+        url: 'https://ahocevar.com/geoserver/wms',
+        params: {'LAYERS': 'topp:states', 'TILED': true},
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,
+      });
 
-  const osni = new TileWMS({
-    url: 'https://services.spatialni.gov.uk/opendata/services/OSNIVector/OSNIOpenData_CoverageGrids/MapServer/WMSServer',
-    params: {'LAYERS': '0,1', 'TILED': true},
-    serverType: 'geoserver',
-    // Countries have transparency, so do not fade tiles:
-    transition: 0,
-  });
+    const osni = new TileWMS({
+        url: 'https://services.spatialni.gov.uk/opendata/services/OSNIVector/OSNIOpenData_CoverageGrids/MapServer/WMSServer',
+        params: {'LAYERS': '0,1', 'TILED': true},
+        serverType: 'geoserver',
+        // Countries have transparency, so do not fade tiles:
+        transition: 0,
+    });
 
 
+    const updateCord = (newCord) => {
+        console.log(geojsonObject2.features[0].geometry.coordinates[0][0]);
+        console.log(newCord)
 
+    }
+
+    console.log(object2);
+    console.log("render")
 
   return (
-      <div>
-        <Map center={fromLonLat(center)} zoom={zoom}>
-          <Layers>
-            <TileLayer source={osm()} zIndex={0} />
-            <TileLayer source={states} zIndex={0} />
+    <div>
+      <Map updateCord={updateCord} center={fromLonLat(center)} zoom={zoom}>
+        <Layers>
+          <TileLayer source={osm()} zIndex={0} />
+          {/*<TileLayer source={states} zIndex={0} />*/}
             <TileLayer source={osni} zIndex={100} />
-            {showLayer1 && (
-                <VectorLayer
-                    source={vector({
-                      features: new GeoJSON().readFeatures(geojsonObject, {
-                        featureProjection: get("EPSG:3857"),
-                      }),
-                    })}
-                    style={FeatureStyles.MultiPolygon}
-                />
-            )}
-            {showLayer2 && (
-                <VectorLayer
-                    source={vector({
-                      features: new GeoJSON().readFeatures(geojsonObject2, {
-                        featureProjection: get("EPSG:3857"),
-                      }),
-                    })}
-                    style={FeatureStyles.MultiPolygon}
-                />
-            )}
-            {showMarker && <VectorLayer source={vector({ features })} />}
-          </Layers>
-          <Controls>
-            <FullScreenControl />
-          </Controls>
-        </Map>
-        <div>
-          <input
-              type="checkbox"
-              checked={showLayer1}
-              onChange={(event) => setShowLayer1(event.target.checked)}
-          />{" "}
-          Dan House
-        </div>
-        <div>
-          <input
-              type="checkbox"
-              checked={showLayer2}
-              onChange={(event) => setShowLayer2(event.target.checked)}
-          />{" "}
-          Glen House
-        </div>
-        <hr />
-        <div>
-          <input
-              type="checkbox"
-              checked={showMarker}
-              onChange={(event) => setShowMarker(event.target.checked)}
-          />{" "}
-          Show markers
-        </div>
+          {showLayer1 && (
+            <VectorLayer
+              source={vector({
+                features: new GeoJSON().readFeatures(geojsonObject, {
+                  featureProjection: get("EPSG:3857"),
+                }),
+              })}
+              style={FeatureStyles.MultiPolygon}
+            />
+          )}
+          {showLayer2 && (
+            <VectorLayer
+              source={vector({
+                features: new GeoJSON().readFeatures(object2, {
+                  featureProjection: get("EPSG:3857"),
+                }),
+              })}
+              style={FeatureStyles.MultiPolygon}
+            />
+          )}
+          {showMarker && <VectorLayer source={vector({ features })} />}
+        </Layers>
+        <Controls>
+          <FullScreenControl />
+        </Controls>
+      </Map>
+      <div>
+        <input
+          type="checkbox"
+          checked={showLayer1}
+          onChange={(event) => setShowLayer1(event.target.checked)}
+        />{" "}
+        Dan House
       </div>
+      <div>
+        <input
+          type="checkbox"
+          checked={showLayer2}
+          onChange={(event) => setShowLayer2(event.target.checked)}
+        />{" "}
+        Glen House
+      </div>
+      <hr />
+      <div>
+        <input
+          type="checkbox"
+          checked={showMarker}
+          onChange={(event) => setShowMarker(event.target.checked)}
+        />{" "}
+        Show markers
+      </div>
+    </div>
   );
 };
 
